ad55db37933b576f4bfb81923bffa74acd2b2350
Orest Yakymiv <akimivorest@gmail.com>
2025-05-21 12:28:28 +0300

    Change path for file

--- scripts/task.ps1
+++
@@ -1,7 +0,0 @@
-# Write your code here
-$ResourceGroup = Get-AzDisk -ResourceGroupName "mate-azure-task-2-ukwest"
-foreach($diskstate in $ResourceGroup) {
-  if($diskstate.DiskState -eq "Unattached") {
-    $diskstate | ConvertTo-Json | Out-File -Path ./result.json
-  }
-}
